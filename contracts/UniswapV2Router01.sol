75312bde8ca57ead70224bff02c7335866d942e9
hanhvn <vonhathanh@gmail.com>
2021-07-04 12:48:15 +0700

    change import path to @defi-warrior

--- contracts/UniswapV2Router01.sol
+++ contracts/UniswapV2Router01.sol
@@ -1,7 +1,7 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
+import '@defi-warrior/core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@defi-warrior/libs/contracts/utils/TransferHelper.sol';
 
 import './libraries/UniswapV2Library.sol';
 import './interfaces/IUniswapV2Router01.sol';
--- contracts/UniswapV2Router02.sol
+++ contracts/UniswapV2Router02.sol
@@ -1,7 +1,8 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
+import '@defi-warrior/core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@defi-warrior/libs/contracts/utils/TransferHelper.sol';
+import '@defi-warrior/core/contracts/interfaces/IUniswapV2Pair.sol';
 
 import './interfaces/IUniswapV2Router02.sol';
 import './libraries/UniswapV2Library.sol';
